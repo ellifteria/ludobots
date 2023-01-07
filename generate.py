aeadbd96ff69207e1c138f78aefbef8154dc035e
Alex Beres <75806636+alexeberes@users.noreply.github.com>
2023-01-07 15:59:38 -0600

    add two boxes to simulation

--- generate.py
+++ generate.py
@@ -1,15 +1,17 @@
 import pyrosim.pyrosim as pyrosim
 
 length = 1
-width = 2
-height = 3
+width = 1
+height = 1
 
 x = 0
 y = 0
 z = height/2
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
 pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
+
+pyrosim.Send_Cube(name="Box2", pos=[x+length, y, z+height], size=[length, width, height])
 
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -14,7 +14,7 @@ p.setGravity(0,0,-9.8)
 
 planeID = p.loadURDF("plane.urdf")
 
-p.loadSDF("box.sdf")
+p.loadSDF("boxes.sdf")
 
 for i in range(1000):
     p.stepSimulation()
