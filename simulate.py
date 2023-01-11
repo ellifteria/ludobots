b73e5c8310f120b60cd6de82ff296941ec55e10e
Alex Beres <75806636+alexeberes@users.noreply.github.com>
2023-01-11 17:28:35 -0600

    change while loop back to for loop in simulation

--- simulate.py
+++ simulate.py
@@ -21,35 +21,19 @@ robot_id = pblt.loadURDF("body.urdf")
 
 pblt.loadSDF("box.sdf")
 
-time_quit = True
 num_iterations = 500
-iteration = 0
 backleg_sensor_vals = np.zeros(num_iterations)
 frontleg_sensor_vals = np.zeros(num_iterations)
 
 pyrosim.Prepare_To_Simulate(robot_id)
 
-while True:
-    if time_quit:
-        if iteration >= num_iterations:
-            break
-
+for i in range(num_iterations):
     pblt.stepSimulation()
 
-    if time_quit:
-        backleg_sensor_vals[iteration] = pyrosim.Get_Touch_Sensor_Value_For_Link("backleg")
-        frontleg_sensor_vals[iteration] = pyrosim.Get_Touch_Sensor_Value_For_Link("frontleg")
-        print(backleg_sensor_vals[iteration])
-        print(frontleg_sensor_vals[iteration])
-    
-    else:
-        backleg_sensor_val = pyrosim.Get_Touch_Sensor_Value_For_Link("backleg")
-        frontleg_sensor_val = pyrosim.Get_Touch_Sensor_Value_For_Link("frontleg")
-        print(backleg_sensor_val)
-        print(frontleg_sensor_val)
-    
-    if time_quit:
-        iteration += 1
+    backleg_sensor_vals[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("backleg")
+    frontleg_sensor_vals[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("frontleg")
+    print(backleg_sensor_vals[i])
+    print(frontleg_sensor_vals[i])
 
     time.sleep(0.05/60)
 
