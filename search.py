956f75255b6f76d555d722838a37d9a5a7a4659d
Alex Beres <75806636+alexeberes@users.noreply.github.com>
2023-01-23 14:39:58 -0600

    video filmed

--- search.py
+++ search.py
@@ -1,9 +1,10 @@
 import os
 
+from simulation import Simulation
+
 for i in range(5):
     os.system("python generate.py")
 
-    from simulation import Simulation
-
     simulation = Simulation()
     simulation.run()
+    del simulation
