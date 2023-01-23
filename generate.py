2f7e0fe1cf244c8cb75ba426d4b86de0c0795971
Alex Beres <75806636+alexeberes@users.noreply.github.com>
2023-01-23 14:43:08 -0600

    video filmed

--- generate.py
+++ generate.py
@@ -44,22 +44,22 @@ def generate_brain():
     pyrosim.Send_Synapse(
         sourceNeuronName='0',
         targetNeuronName='3',
-        weight=1.0
+        weight=-1.0
     )
     pyrosim.Send_Synapse(
         sourceNeuronName='1',
         targetNeuronName='3',
-        weight=1.0
+        weight=-1.0
     )
     pyrosim.Send_Synapse(
         sourceNeuronName='0',
         targetNeuronName='4',
-        weight=1.0
+        weight=-1.0
     )
     pyrosim.Send_Synapse(
         sourceNeuronName='2',
         targetNeuronName='4',
-        weight=1.0
+        weight=-1.0
     )
     pyrosim.End()    
 
