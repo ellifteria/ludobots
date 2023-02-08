b1f619b1f5ba5e5798fa561421c4513b54fd33a4
Alex Beres <75806636+alexeberes@users.noreply.github.com>
2023-02-08 10:12:04 -0600

    renamed files

--- search.py
+++ search.py
@@ -1,18 +1,18 @@
 import argparse
-from p_fae1 import P_FAE_1
-from f_fae1 import F_FAE_1
+from ludofae1 import ludoFAE1
+from ludofae2 import ludoFAE2
 
 parser = argparse.ArgumentParser(
     prog = 'search.py',
     description = 'Performs a search for an optimizied neural network weight set for a simulated robot using FAEry Algorithms')
-parser.add_argument('-m', '--method', choices=['p_fae_1', 'f_fae_1'], default='p_fae_1')
+parser.add_argument('-m', '--method', choices=['ludoFAE1', 'ludoFAE2'], default='ludoFAE1')
 
 args = parser.parse_args()
 
-if args.method == 'p_fae_1':
-    evolutionary_algorithm = P_FAE_1()
-elif args.method == 'f_fae_1':
-    evolutionary_algorithm = F_FAE_1()
+if args.method == 'ludoFAE1':
+    evolutionary_algorithm = ludoFAE1()
+elif args.method == 'ludoFAE2':
+    evolutionary_algorithm = ludoFAE2()
 else:
     exit()
 
